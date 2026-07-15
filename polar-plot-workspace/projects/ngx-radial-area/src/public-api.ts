a66b585d11452aa3da1ab202d0c37fb4c7fc1a76
DevangBarot-BH <108133334+DevangBarot-BH@users.noreply.github.com>
2026-07-15 09:25:14 +0530

    fix(000000): remove old code

--- polar-plot-workspace/projects/ngx-radial-area/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-radial-area
- */
-
-export * from './lib/radial-area.models';
-export * from './lib/radial-area.service';
-export * from './lib/ngx-radial-area.component';
